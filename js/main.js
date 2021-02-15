b276a5898821ceda5d07a17920d1c4864eecffeb
Ulisses Willian Reichenbach <uliwill@gmail.com>
2021-02-15 04:33:58 -0300

    Javascript load movies

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(function () {
+    $("#conteudo").load("./filmes/avengers3.html");
+})
+
+function CarregaPage(page) {
+    //console.log(page);
+    $("#conteudo").load(page);
+    window.event.preventDefault();
+}
+
+
 function navResponsivo() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
--- js/owl/setup.js
+++ js/owl/setup.js
@@ -5,7 +5,28 @@ $('#carousel-1').owlCarousel({
 });
 
 $('#carousel-2').owlCarousel({
-    loop: true,
+    loop: false,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 5,
+            nav: false,
+            loop: false
+        }
+    }
+})
+
+$('#carousel-3').owlCarousel({
+    loop: false,
     margin: 10,
     responsiveClass: true,
     responsive: {
